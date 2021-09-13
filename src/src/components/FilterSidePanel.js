7d94d7c0fdf1ba0e2479b85b90498bd3fd690cd1
Yassin Siouda <yassin@siouda.com>
2021-09-13 08:14:35 +0200

    FEAT Filter styling

--- src/src/components/FilterSidePanel.js
+++ src/src/components/FilterSidePanel.js
@@ -4,6 +4,7 @@ import { Provider, useDispatch, connect } from "react-redux";
 import {setFilter} from "kepler.gl/actions";
 
 import Button from './filter-side-panel/Button'
+import styled from 'styled-components';
 
 
 
@@ -11,6 +12,16 @@ const FilterSidePanel = () => {
 
     
     const dispatch = useDispatch()
+    const [togglestate, setTogglestate] = useState("false") 
+
+
+    const switchToggle = () => {
+        console.log("HOHOHO")
+        setTogglestate(!togglestate)
+        console.log(togglestate)
+    }
+
+
     const [filtercat, setFiltercat] = useState("hi");
     useEffect(() => {
         dispatch(setFilter(0,"value", [filtercat]))
@@ -22,25 +33,25 @@ const FilterSidePanel = () => {
         <div className='filters'>
         <ul>
             <li id="filter-parent-1" className="filter-parent">
-                <button>Structures Mediation</button>
-                    <ul>
+                <button onClick={(e) => switchToggle}>Structures Mediation</button>
+                    <ul className={togglestate ? 'active' : ''}>
                         <li className="filter-child"><button>Types de structures</button></li>
                         <li className="filter-child"><button>Publics concernes</button></li>
                         <li className="filter-child">
-                            <button>Types d'activites</button>
                             <ul>
-                                <li><Button text="Accompagnement à l'innovation" onClick={(e) => setFiltercat("{\"Accompagnement à l'innovation\"}")} /></li>
-                                <li><Button text="Anim event réseaux " onClick={(e) => setFiltercat("{\"Animation / événements / réseaux\"}")} /></li>
+                                <li><Button fontSize="12" text="Accompagnement à l'innovation" onClick={(e) => setFiltercat("{\"Accompagnement à l'innovation\"}")} /></li>
+                                <li><Button fontSize="12" text="Anim event réseaux " onClick={(e) => setFiltercat("{\"Animation / événements / réseaux\"}")} /></li>
                                 <li> <button>Activité 2</button></li>
                             </ul>
+                            <button>Types d'activites</button>
                             </li>
                     </ul>
                 
                 </li>
 
             <li id="filter-parent-2" className="filter-parent">
-                <button>Evenements</button>
-                <ul>
+                <button onClick={switchToggle}>Evenements</button>
+                <ul className={togglestate ? 'active' : ''}>
                     <li className="filter-child"><button>Conférences</button></li>
                     <li className="filter-child"><button>Ateliers</button></li>
                     <li className="filter-child"><button>Portes ouvertes</button></li>
