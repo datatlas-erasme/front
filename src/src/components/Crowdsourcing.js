95bd911a46eca47425f555118a40010bf67fb6cf
Yassin Siouda <yassin@siouda.com>
2021-09-15 19:00:18 +0100

    REFACT crowsourcing

--- src/src/components/Crowdsourcing.js
+++ src/src/components/Crowdsourcing.js
@@ -1,20 +1,11 @@
 import React, { }  from 'react';
+import Button from './filter-side-panel/Button'
 
 const Crowdsourcing = () => {
     return (
-        <div>
-            <a href="https://form.typeform.com/to/ijzY76oa">
-                <h1 style={{ 
-                    fontSize: "20px", 
-                    position: "absolute", 
-                    bottom: 0, 
-                    right: "20px",
-                    backgroundColor: "black",
-                    color: "white",
-                    padding: "15px",
-                    borderRadius: "12px"
-                }}> + Proposer un lieu</h1>
-            </a>
+        <div className="crowdsourcing">
+            <Button bg="black" fontSize="10" text=" + Ajouter un lieu" />
+            <Button bg="black" fontSize="10" text=" + Ajouter un événement" />
         </div>
         
     )
