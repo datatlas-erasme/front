aad843d6b10f7d7e011b320cc385072a7b54f2e0
Yassin Siouda <yassin@siouda.com>
2021-09-20 13:39:13 +0200

    FEAT updated crowdsourcing urls

--- src/src/components/Crowdsourcing.js
+++ src/src/components/Crowdsourcing.js
@@ -4,8 +4,8 @@ import Button from './filter-side-panel/Button'
 const Crowdsourcing = () => {
     return (
         <div className="crowdsourcing">
-            <Button bg="black" fontSize="10" text=" + Ajouter un lieu" />
-            <Button bg="black" fontSize="10" text=" + Ajouter un événement" />
+            <a href="https://form.typeform.com/to/V1f3GNXR"><Button bg="black" fontSize="10" text=" + Ajouter un lieu" /></a>
+            <a href="https://form.typeform.com/to/vTRYrYpv?typeform-source=tank-tiga.webflow.io"><Button bg="black" fontSize="10" text=" + Ajouter un événement" /></a>
         </div>
         
     )
