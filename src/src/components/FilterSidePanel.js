a95d8bb773441cc8cfedc2d52f1d19bc645ddff9
Yassin Siouda <yassin@siouda.com>
2021-09-21 19:51:05 +0200

    FIX removed bugged featured for release

--- src/src/components/FilterSidePanel.js
+++ src/src/components/FilterSidePanel.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import {Provider, useDispatch, connect } from "react-redux";
 
-import {setFilter, removeLayer} from "kepler.gl/actions";
+import {setFilter, layerVisConfigChange} from "kepler.gl/actions";
 
 import Button from './filter-side-panel/Button'
 import styled from 'styled-components';
 
+
+import mapConfig from '../static/defaultDisplayConf.json';
 
 
 const FilterSidePanel = () => {
@@ -15,24 +17,23 @@ const FilterSidePanel = () => {
     const switchparent2 = () => {setParent2(!parent2)}
 
 
-
-    const dispatch = useDispatch()
-    const [filtersarray, setFiltersarray] = useState([])
-    //const [filtercat, setFiltercat] = useState();
-    const [togglestate, setTogglestate] = useState("true") 
-
     const [parent1, setParent1] = useState("false") 
     const [parent2, setParent2] = useState("false") 
 
+    const dispatch = useDispatch()
+    const [filtersarray, setFiltersarray] = useState([])
 
-    //const [filtercat, setFiltercat] = useState("hi");
-    //useEffect(() => {
+    const [togglestate, setTogglestate] = useState("true") 
+
+    //const [filtercat, setFiltercat] = useState();
+    const [filtercat, setFiltercat] = useState("hi");
+    useEffect(() => {
         
         //let finalArray = filtersarray.includes(filtercat) 
 
        
 
-        //dispatch(setFilter(0,"value", filters))
+        dispatch(setFilter(0,"value", [filtercat]))
         /*filters.map((item) => {
             dispatch(setFilter(0,"value", [item]))
         })*/
@@ -42,10 +43,10 @@ const FilterSidePanel = () => {
         //dispatch(setFilter(0,"value", filtersarray))
         //dispatch(setFilter(0,"value", ["{\"Accompagnement à l'innovation\"}","{\"Animation / événements / réseaux\"}"]))
 
-    //}, )
+    }, [filtercat])
 
     const test = (filtercat) => {
-        if (filtersarray.includes(filtercat)) {
+        /*if (filtersarray.includes(filtercat)) {
             console.log("Already in array")
             setFiltersarray(filtersarray.filter((cat)=>{ return cat != filtercat }))
             
@@ -55,13 +56,15 @@ const FilterSidePanel = () => {
         else {
             setFiltersarray(prevArray => [...prevArray, filtercat])
             console.log(filtersarray)
-        }
-        dispatch(setFilter(0,"value", filtersarray))
+        }*/
+        dispatch(setFilter(0,"value", filtercat))
 
     }
 
+    //console.log(mapConfig.config.visState.layers[0])
+
     const disableLayer = () => {
-        dispatch(removeLayer(1))
+        //dispatch(layerVisConfigChange(mapConfig.config.visState.layers[0], {isVisible:false}))
     }
 
 
@@ -72,19 +75,17 @@ const FilterSidePanel = () => {
                 <button onClick={switchparent1}>Structures Mediation</button>
                     <ul className={!parent1 ? 'active' : ''}>
                         <li className="filter-child"><Button textSize="12px"  bg="#d91f16" text="Types de structures" /></li>
-                        <li className="filter-child"><Button textSize="12px"  bg="#d91f16" text="Publics concernes" /></li>
                         <li className="filter-child">
                             <ul className={!parent1 ? 'active' : ''}>
-                                <li><Button textSize="10px" bg="#d91f16" text="Association ou syndicat professionnel" onClick={(e) => test("Association ou syndicat professionnel")} /></li>
-                                <li><Button textSize="10px" bg="#d91f16" text="Autre" onClick={(e) => test("Autre")} /></li>
-                                <li><Button textSize="10px" bg="#d91f16" text="Ecole / université / enseignement supérieur" onClick={(e) => test("Ecole / université / enseignement supérieur")} /></li>
-                                <li><Button textSize="10px" bg="#d91f16" text="Entreprise de droit privé ou fondation" onClick={(e) => test("Entreprise de droit privé ou fondation")} /></li>
-                                <li><Button textSize="10px" bg="#d91f16" text="Structure publique ou parapublique" onClick={(e) => test("Structure publique ou parapublique")} /></li>
+                                <li><Button textSize="10px" bg="#d91f16" text="Association ou syndicat professionnel" onClick={(e) => setFiltercat("Association ou syndicat professionnel")} /></li>
+                                <li><Button textSize="10px" bg="#d91f16" text="Autre" onClick={(e) => setFiltercat("Autre")} /></li>
+                                <li><Button textSize="10px" bg="#d91f16" text="Ecole / université / enseignement supérieur" onClick={(e) => setFiltercat("Ecole / université / enseignement supérieur")} /></li>
+                                <li><Button textSize="10px" bg="#d91f16" text="Entreprise de droit privé ou fondation" onClick={(e) => setFiltercat("Entreprise de droit privé ou fondation")} /></li>
+                                <li><Button textSize="10px" bg="#d91f16" text="Structure publique ou parapublique" onClick={(e) => setFiltercat("Structure publique ou parapublique")} /></li>
 
                             </ul>
 
                             </li>
-                        <li className="filter-child"><Button textSize="12px"  bg="#d91f16" text="Types d'activites" /></li>
                     </ul>
                 </li>
 
@@ -96,7 +97,6 @@ const FilterSidePanel = () => {
                     <li className="filter-child"><button>Portes ouvertes</button></li>
                 </ul>
             </li>
-            <li className="filter-child"><Button onClick={disableLayer} bg="black" text="Calque de données" /></li>
         </ul>
     </div>
     )
