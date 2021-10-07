7ecf9706f4576046c802bc2cd8080a5d19265601
Yassin Siouda <yassin@siouda.com>
2021-10-07 11:00:38 +0200

    FEAT WIP Icon param on button component

--- src/src/components/filter-side-panel/Button.js
+++ src/src/components/filter-side-panel/Button.js
@@ -1,12 +1,27 @@
 import React, {}  from 'react';
 import PropTypes from 'prop-types'
+import ScatterplotIconLayer from "kepler.gl"
 
-const Button = ({text,bg,textSize, onClick}) => {
+
+/*const SvgIcon = () => {[
+    new ScatterplotIconLayer({
+        id: "place",
+      }),
+]}*/
+
+const Button = ({text,bg,textSize, onClick, icon, borderColor}) => {
     return  (
     <button 
     onClick={onClick}
     style={{backgroundColor : bg , fontSize : textSize}} 
-    className="btn">{text}</button>
+    className="btn">
+    <span style={{backgroundColor : borderColor, }}>{icon}</span>
+    {text}
+    </button>
+
+
+
+
     )
 }
 
@@ -21,7 +36,9 @@ Button.propTypes = {
     bg: PropTypes.string,
     fontSize: PropTypes.string,
     action: PropTypes.object,
-    onClick: PropTypes.func
+    onClick: PropTypes.func,
+    icon: PropTypes.string,
+    borderColor: PropTypes.string
 }
 
 export default Button
