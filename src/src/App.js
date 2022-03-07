68dfd132e2827a9910357830f5f307fff8cd2aa5
Yassin Siouda <yassin@siouda.com>
2022-03-07 14:26:00 +0100

    Update industrie build

---
+++ src/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const Back = React.lazy(() => import('./Back'));
+const Front = React.lazy(() => import('./Front'));
+document.title = "M.Ind."
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="*"
+          element={
+            <React.Suspense fallback={null}>
+              <Front />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/back"
+          element={
+            <React.Suspense fallback={null}>
+              <Back />
+            </React.Suspense>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
