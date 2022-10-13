5c08485c61b153f8f51127e813dcd03d0c1bd601
Olivier <olivier@opierre.fr>
2022-10-13 17:06:55 +0200

    STYLE: theming sidepanel

--- src/components/panel-control/style.ts
+++ src/components/panel-control/style.ts
@@ -1,28 +1,21 @@
 import styled from 'styled-components';
-import { device } from '../../styles/breakpoints';
+import { device } from '../../styles';
 import px2vw from '../../utils/px2vw';
-
-// get theme from env var or default to datalimentaire
-// const theme = industries ;
-
-// const theme = instance.conf?.theme?.name === 'Industries' ? industries : datalimentaire;
 
 export const Panel = styled.div`
   display: none;
-  display: flex;
   flex-flow: column nowrap;
   padding-top: ${px2vw(40)};
 
   @media ${device.lg} {
-    justify-content: center;
-    flex-direction: column;
-    position: relative;
-    background-color: ${({ theme }) => theme.colors.secondary};
-    top: 0;
-    bottom: 0;
+    display: flex;
+    position: ${({ theme }) => theme.Panel.position};
+    right: ${({ theme }) => theme.Panel.right};
     width: 45vw;
     height: 100vh;
-    padding-top: 0px;
+    justify-content: ${({ theme }) => theme.Panel.justifyContent};
+    flex-direction: column;
+    background-color: ${({ theme }) => theme.colors.background};
   }
   @media ${device.xl} {
     width: 35vw;
