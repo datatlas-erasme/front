e755d1f1af17db4d5a4da25c05395fc1c79a0110
Yassin Siouda <yassin@siouda.com>
2022-11-14 14:41:43 +0100

    FIXED default env injection

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #set -e
 
-
 echo "Writing env var into file "
 #echo $REACT_APP_BACKEND_URL
-echo "REACT_APP_BACKEND_URL='"http://localhost:3000"'" > .env 
+echo "REACT_APP_BACKEND_URL=localhost:3000" > .env 
 
 
 exec "$@"
