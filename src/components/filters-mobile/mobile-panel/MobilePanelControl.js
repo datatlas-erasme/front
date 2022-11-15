384dbe1bdf6d1d176192e36231e90a60863a7b1d
Olivier <olivier@opierre.fr>
2022-11-15 17:26:40 +0100

    Fix style mobile version

--- src/components/filters-mobile/mobile-panel/MobilePanelControl.js
+++ src/components/filters-mobile/mobile-panel/MobilePanelControl.js
@@ -12,7 +12,6 @@ const MobilePanelControl = ({ closeOtherItemsOnClick, filtersDomain, initialActi
 
       return;
     }
-
     let newActiveItemIndexes = [activeItemIndexes];
     if (newIndexes) {
       newActiveItemIndexes = newActiveItemIndexes.filter((item) => item !== index);
--- src/components/panel-control/style.ts
+++ src/components/panel-control/style.ts
@@ -3,25 +3,21 @@ import { device } from '../../styles';
 import px2vw from '../../utils/px2vw';
 
 export const Panel = styled.div`
-  display: none;
+  display: flex;
   flex-flow: column nowrap;
   padding-top: ${px2vw(40)};
 
   @media ${device.lg} {
-    display: flex;
     position: ${({ theme }) => theme.Panel.position};
-    right: ${({ theme }) => theme.Panel.right};
     display: flex;
-    justify-content: center;
     flex-direction: column;
+    justify-content: ${({ theme }) => theme.Panel.justifyContent};
+    right: ${({ theme }) => theme.Panel.right};
     background-color: ${({ theme }) => theme.colors.secondary};
     top: 0;
     bottom: 0;
     width: 45vw;
     height: 100vh;
-    justify-content: ${({ theme }) => theme.Panel.justifyContent};
-    flex-direction: column;
-    background-color: ${({ theme }) => theme.colors.background};
   }
   @media ${device.xl} {
     width: 35vw;
